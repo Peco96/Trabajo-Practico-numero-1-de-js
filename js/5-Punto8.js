305673926d8c36bdadf92abfcaac15070a1b315f
´Peco´ <pecohernandez6@gmail.com>
2022-07-06 17:16:34 -0300

    Punto8

---
+++ js/5-Punto8.js
@@ -0,0 +1,9 @@
+//punto8
+
+let numero1 = parseInt(prompt("ingresar un primer numero"));
+console.log(numero1);
+if ((numero1 % 2) === 0){
+    document.write ("El numero es divisible en 2: " + numero1);
+} else{
+    document.write ("El numero no es divisible en 2: " + numero1);
+}
