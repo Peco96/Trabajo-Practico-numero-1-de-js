d5de55d68a0264227ad9fae954b9168d71f3a90f
´Peco´ <pecohernandez6@gmail.com>
2022-07-06 17:12:05 -0300

    Puntos1-2-3-4

---
+++ js/1-Puntos1-2-3-4.js
@@ -0,0 +1,12 @@
+//Punto 1
+console.log("un mensaje");
+//Punto 2
+document.write("<h1>Hello World</h1>");
+//Punto3
+document.write("resultado "+ (3 + 5));
+//Punto 4
+let nombrePersona = prompt("Ingrese el nombre de usuario");
+//En consola
+console.log("Hola " + nombrePersona);
+//En pantalla
+document.write("<p> Hola  "+ nombrePersona + "</p>" );
