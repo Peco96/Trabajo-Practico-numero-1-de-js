a2532d849888c80800808f6e464c0b8a73ffbbc6
´Peco´ <pecohernandez6@gmail.com>
2022-07-06 17:13:02 -0300

    Punto5

---
+++ js/2-Punto5.js
@@ -0,0 +1,4 @@
+//Punto 5
+let numero1 = parseInt(prompt("Ingrese el primer numero"));
+let numero2 = parseInt(prompt("Ingrese el segundo numero"));
+document.write("Resultado es: " + (numero1 + numero2));
